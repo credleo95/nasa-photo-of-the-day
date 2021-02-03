36cd8c043a9e83b2f39096e31847c59140fb2b68
Omarius Credle <ocredle@omariuss-mbp.home>
2021-02-03 00:17:39 -0500

    prepped sketch for page and started creating components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
+function Input() {
+  /* STEP 1 */
+  const [inputValue, setInputValue] = useState("")
+  const changeInput = evt => {
+    
+    const value = evt.target.value;
+     setInputValue(value);
+  };
+  const reset = () => {
+     setInputValue("")
+  }
+}
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Traverse The Heavens Below...</h1>
+      <img src="https://apod.nasa.gov/apod/image/2102/MeteorStreak_Kuszaj_5341.jpg" alt="Nasa-image"/>
+       <div className ="flex-container">
+      <button className ='search'>Search</button>
+      <button className='surprise'>Surprise Me!</button>
+      </div>
     </div>
   );
 }
